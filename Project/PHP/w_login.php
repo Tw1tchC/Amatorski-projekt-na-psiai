ef1f4d9e27a4650be0c2dd7192134d3831691f94
Tw1tchC <113410216+Tw1tchC@users.noreply.github.com>
2023-06-02 19:17:05 +0200

    Update w_login.php

--- Project/PHP/w_login.php
+++ Project/PHP/w_login.php
@@ -1,117 +1,89 @@
-<html lang="pl">
+<?php
+// Rozpoczyna sesje
+session_start();
 
+// połączenia z bazą danych
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "hungryhub";
+
+// Utwórz nowe połączenie
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Sprawdź połączenie
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Sprawdza czy uzytkownik jest juś zalogowany jesli tak to odrazu przekierowuje do strony 
+if (isset($_SESSION["user_id"])) {
+    // to przekieruje do strony którą bedziesz chciał narazie dam tu jakies dashboard zjebane
+    header("Location: dashboard.php");
+    exit();
+}
+
+// formularz został przesłany
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Pobierz dane formularza
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+
+    // SQL
+    $sql = "SELECT * FROM dane_uzytkownikow WHERE login = ? AND password = ?";
+
+    // statement
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $login, $password);
+
+    // Execute the statement
+    $stmt->execute();
+
+    // wynik
+    $result = $stmt->get_result();
+
+    // Sprawdź, czy użytkownik istnieje
+    if ($result->num_rows > 0) {
+        // Pobierz dane użytkownika
+        $user = $result->fetch_assoc();
+
+        // Zapisuje id i te tegetsy
+        $_SESSION["user_id"] = $user["user_id"];
+        $_SESSION["login"] = $user["login"];
+
+        // to przekieruje do strony którą bedziesz chciał narazie dam tu jakies dashboard zjebane
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        $error = "Bład podczas logowania, hasło lub login są nieprawidłowe!";
+    }
+
+    //statement
+    $stmt->close();
+}
+
+// the connection
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/w_login.css">
-    <link rel="shortcut icon" type="x-icon" href="../IMG/HH_miniaturka.png">
-    <title>HungryHub-Logowanie</title>
+    <title>User Login</title>
 </head>
-
 <body>
-    <header>
-        <div>
-            <a href="../w_main.html"><img src="../IMG/HH_logo.png" class="img1"></a>
-        </div>
-        <nav id="topnav">
-            <ul class="menu">
-                <li><a href="../PHP/w_register.php">Rejestracja</a></li>
-                <li><a href="../PHP/w_login.php">Logowanie</a></li>
-                <li><a href="">Lista dostępnych restauracji oraz sklepów</a></li>
-                <li><a href="">Koszyk i kasa</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <?php
-    session_start();
-
-    // Sprawdza czy formularz został spostowany 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // zbiera dane z formularza nie przejmuj sie sanityzacją bo to jest tylko polega na usuwaniu potencjalnie złośliwych kawałków tam plików bez utraty ich treści
-        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
-
-        $errors = [];
-
-        if (empty($email)) {
-            $errors[] = 'Email is required';
-        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = 'Email is invalid';
-        }
-
-        if (empty($password)) {
-            $errors[] = 'Password is required';
-        }
-
-        // jeśli nie ma błędu to sprawdza połączenia
-        if (empty($errors)) {
-
-            $mysqli = new mysqli('localhost', 'root', '', 'php_project_by_adrian_kulik');
-
-            if ($mysqli->connect_error) {
-                echo 'Failed to connect to MySQL: ' . $mysqli->connect_error;
-                exit();
-            }
-
-            // przygotowuje zapytanie
-            $stmt = $mysqli->prepare('SELECT id, username, password FROM users WHERE email = ?');
-            $stmt->bind_param('s', $email);
-
-            // zapytanie
-            if ($stmt->execute()) {
-                // rezultat
-                $result = $stmt->get_result();
-
-                // jesli użytkownik istnieje hasło jest poprawne 
-                if ($result->num_rows === 1) {
-                    $row = $result->fetch_assoc();
-                    if (password_verify($password, $row['password'])) {
-                        $_SESSION['user_id'] = $row['id'];
-                        $_SESSION['username'] = $row['username'];
-
-                        header('Location: main.html');
-                        exit();
-                    } else {
-                        $errors[] = 'Incorrect password';
-                    }
-                } else {
-                    $errors[] = 'User not found';
-                }
-            } else {
-                echo 'Failed to retrieve user information from database: ' . $stmt->error;
-            }
-
-            $stmt->close();
-            $mysqli->close();
-        }
-    }
-    ?>
-    <?php
-    // sprawdza czy nie ma błędów
-    if (!empty($errors)) {
-        echo '<ul>';
-        foreach ($errors as $error) {
-            echo '<li>' . $error . '</li>';
-        }
-        echo '</ul>';
-    }
-    ?>
-    <div class="logowanie">
-        <h1>Logowanie</h1>
-        <h2>Wpisz Nazwę oraz Hasło</h2>
-        <form action="w_login.php" method="post">
-            <label for="email">Email:</label><br>
-            <input type="email" id="email" name="email" required><br>
-
-            <label for="password">Password:</label><br>
-            <input type="password" id="password" name="password" required><br><br>
-
-            <button type="submit" class="button1">Login</button>
-        </form>
-    </div>
-
+    <h2>User Login</h2>
+    <?php if (isset($error)) { ?>
+        <p><?php echo $error; ?></p>
+    <?php } ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label>Login:</label>
+        <input type="text" name="login" required><br><br>
+        
+        <label>Password:</label>
+        <input type="password" name="password" required><br><br>
+        
+        <input type="submit" value="Login">
+    </form>
 </body>
-
 </html>
