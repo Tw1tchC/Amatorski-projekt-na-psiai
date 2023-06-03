3fe462591f360cd72c0f914f8c9dcb4d2334d69b
Mis13k <129774859+Mis13k@users.noreply.github.com>
2023-06-03 15:04:07 +0200

    Update w_register.php

--- Project/PHP/w_register.php
+++ Project/PHP/w_register.php
@@ -1,3 +1,63 @@
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/w_register.css">
+    <link rel="shortcut icon" type="x-icon" href="../IMG/HH_miniaturka.png">
+    <title>HungryHub-Rejestracja</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <a href="../w_main.html"><img src="../IMG/HH_logo.png" class="img1"></a>
+        </div>
+        <nav id="topnav">
+            <ul class="menu">
+                <li><a href="../PHP/w_register.php">Rejestracja</a></li>
+                <li><a href="../PHP/w_login.php">Logowanie</a></li>
+                <li><a href="">Lista dostępnych restauracji oraz sklepów</a></li>
+                <li><a href="">Koszyk i kasa</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div>
+        <div class="block1">
+            <h1>Zarejestruj się</h1>
+            <div id="dologowania">
+                <button type="button" class="button_1">
+                    <a href="w_login.php" target="_blank">Już posiadasz konto? Zaloguj się tutaj!</a>
+                </button>
+            </div>
+            <br>
+        </div>
+        <br>
+        <div class="register">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"><br>
+                <label>Login:</label><br>
+                <input type="text" name="login" required><br><br>
+
+                <label>Hasło:</label><br>
+                <input type="password" name="password" required><br><br>
+
+                <label>Adres:</label><br>
+                <input type="text" name="address" required><br><br>
+
+                <label>Email:</label><br>
+                <input type="email" name="email" required><br><br>
+
+                <label>Wiek:</label><br>
+                <input type="number" name="age" required><br><br>
+
+                <input class="button_2" type="submit" value="Register">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
 <?php
 // Database connection parameters
 $servername = "localhost";
@@ -43,32 +103,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 // Close the connection
 $conn->close();
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Registration</title>
-</head>
-<body>
-    <h2>User Registration</h2>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label>Login:</label>
-        <input type="text" name="login" required><br><br>
-        
-        <label>Password:</label>
-        <input type="password" name="password" required><br><br>
-        
-        <label>Address:</label>
-        <input type="text" name="address" required><br><br>
-        
-        <label>Email:</label>
-        <input type="email" name="email" required><br><br>
-        
-        <label>Age:</label>
-        <input type="number" name="age" required><br><br>
-        
-        <input type="submit" value="Register">
-    </form>
-</body>
-</html>
-
