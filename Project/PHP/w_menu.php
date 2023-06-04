227d2ca019939e0ed88ee9fa17e8307443efce42
Mis13k <129774859+Mis13k@users.noreply.github.com>
2023-06-04 21:27:23 +0200

    Update w_menu.php

--- Project/PHP/w_menu.php
+++ Project/PHP/w_menu.php
@@ -1,21 +1,36 @@
 <html lang="pl">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/w_menu.css">
+    <link rel="shortcut icon" type="x-icon" href="../IMG/HH_miniaturka.png">
     <title>Menu</title>
 </head>
 <div>
- 
-<body>
-<div id="my-categories-88109711"></div>
-<div>
-<script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?88109711&data_platform=code&data_date=2023-06-03" charset="utf-8"></script>
-<script type="text/javascript"> xCategoriesV2("id=my-categories-88109711"); </script>
-</div>
-<div id="my-store-88109711"></div>
-<div>
-<script data-cfasync="false" type="text/javascript" src="https://app.ecwid.com/script.js?88109711&data_platform=code&data_date=2023-06-03" charset="utf-8"></script><script type="text/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-88109711");</script>
-</div>
-</body>
+
+    <body>
+        <header>
+            <div>
+                <a href="../w_main.html"><img src="../IMG/HH_logo.png" class="img1"></a>
+            </div>
+        </header>
+        <div id="my-categories-88109711"></div>
+        <div>
+            <script data-cfasync="false" type="text/javascript"
+                src="https://app.ecwid.com/script.js?88109711&data_platform=code&data_date=2023-06-03"
+                charset="utf-8"></script>
+            <script type="text/javascript"> xCategoriesV2("id=my-categories-88109711"); </script>
+        </div>
+        <div id="my-store-88109711"></div>
+        <div>
+            <script data-cfasync="false" type="text/javascript"
+                src="https://app.ecwid.com/script.js?88109711&data_platform=code&data_date=2023-06-03"
+                charset="utf-8"></script>
+            <script
+                type="text/javascript"> xProductBrowser("categoriesPerRow=3", "views=grid(20,3) list(60) table(60)", "categoryView=grid", "searchView=list", "id=my-store-88109711");</script>
+        </div>
+    </body>
+
 </html>
